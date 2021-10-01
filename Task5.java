ab70e9d40c8aa85a2375ae67b4dd640b117eed86
Asaadi0105 <89209258+Asaadi0105@users.noreply.github.com>
2021-09-30 19:07:32 -0500

    Commit Test1

--- Task5.java
+++ Task5.java
@@ -1,22 +1,28 @@
 package Project3;
-
 import java.util.Scanner;
-
 public class Task5 {
     public static void main(String[] args) {
-
+/*
+Using the scanner asks the user to enter one sentence with three words and print the index number
+of each word's last character and then print the sum of each index number that you found.
+For Example:
+Input:
+"Importance of Human" --> it can be any three-word sentence.
+Output:
+9 --> index number of 'e'
+12 --> index number of 'f'
+18 --> index number of 'n'
+The sum: 39
+Description
+ */
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter one sentence with 3 words: ");
 
         String text = input.nextLine();
-//        System.out.println(text);
 
         int word1Index = text.indexOf(' ')-1;
-        //System.out.println(word1Index);
         int word2Index = text.indexOf(' ',text.indexOf(' ')+1)-1;
-        //System.out.println(word2Index);
         int word3Index = text.length()-1;
-       // System.out.println(word3Index);
         char word1LastLetter = text. charAt(word1Index);
         char word2LastLetter = text.charAt(word2Index);
         char word3LastLetter = text.charAt(word3Index);
--- Task7.java
+++ Task7.java
@@ -1,10 +1,19 @@
 package Project3;
-
 import java.util.Locale;
 import java.util.Scanner;
-
 public class Task7 {
     public static void main(String[] args) {
+/*
+Using
+scanner enter String (any two words) value
+with spaces in the beginning and at the end. In this input, the second word is not starting with the upper case,
+using string methods change the letter with upper case and print it
+Example 1:
+Please enter the String with the following format “ word “
+Natural black
+The output is:
+Natural Black
+ */
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the String with the following format: ");
